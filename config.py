ae674f033f49117a1c8dbe606a4b5591e30dda31
Keremcm <keremcem0120@hotmail.com>
2026-04-23 14:01:38 +0300

    CF Trunstile fix.

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ class Config:
         'X-Frame-Options': 'DENY',
         'X-XSS-Protection': '1; mode=block',
         'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://challenges.cloudflare.com ws://127.0.0.1:* wss://127.0.0.1:*; font-src 'self';",
+        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://challenges.cloudflare.com; connect-src 'self' https://challenges.cloudflare.com ws://127.0.0.1:* wss://127.0.0.1:*; font-src 'self'; frame-src https://challenges.cloudflare.com; child-src https://challenges.cloudflare.com;",
         'Referrer-Policy': 'strict-origin-when-cross-origin',
         'Permissions-Policy': 'geolocation=(), microphone=(), camera=()'
     }
